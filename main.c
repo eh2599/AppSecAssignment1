bc972d30bb351b8b972b75d8180a9799b08a1392
Elizabeth <eh2599@nyu.edu>
2020-06-09 19:23:54 -0500

    Committing changes to main and load_dictionary in spell

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "dictionary.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+	//create hashmap to hold dictionary words
+	hashmap_t hashtable[HASH_SIZE];
+
+	printf("\nLoading dictionary\n");
+
+	//load dictionary into hashmap
+	bool success = load_dictionary("wordlist.txt", hashtable);
+
+	if (success)
+	{
+		printf("\nDictionary successfully loaded");
+	}
+	else
+	{
+		printf("\nDictionary failed to load\n");
+	}
+
 	return 0;
 }
--- spell.c
+++ spell.c
@@ -1,6 +1,68 @@
 #include <stdlib.h>
+#include "dictionary.h"
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
-int spell(void) {
+bool check_word(const char* word, hashmap_t hashtable[])
+{
 
-    return 0;
+	return true;
+}
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
+{
+	int i;
+	//Initialize all values in hash table to NULL
+	for (i = 0; i < HASH_SIZE; i++)
+	{
+		hashtable[i] = NULL;
+	}
+
+	//Open dictionary file
+	FILE *dict_file = fopen(dictionary_file, "r");
+
+	//Checking success of opening the file
+	if (dict_file == NULL)
+	{
+		return false;
+	}
+
+	char word[45];
+	int bucket;
+	//Add words from file to hashtable
+	while (fscanf (dict_file, "%43s", word) == 1)
+	{
+		hashmap_t new_node = malloc(sizeof(node));
+		new_node->next = NULL;
+		strcpy(new_node->word, word);
+		bucket = hash_function(word);
+		if (hashtable[bucket] == NULL)
+		{
+			hashtable[bucket] = new_node;
+		}
+		else
+		{
+			new_node->next = hashtable[bucket];
+			hashtable[bucket] = new_node;
+		}
+	}
+	fclose(dict_file);
+	for(i = 0; i < 10; i++)
+	{
+		if (hashtable[i] != NULL)
+		{
+			printf("%s\n", hashtable[i]->word);
+		}
+	}
+
+	return true;
+
+}
+
+int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
+{
+
+	return 0;
 }
